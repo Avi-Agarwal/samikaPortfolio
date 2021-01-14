aae0bd48c0093ac8d386ef9cdeb035d3cf5e0676
Avi Agarwal <avi@habu.com>
2021-01-13 23:50:09 -0800

    Lets go!

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Portfolio = (props) => {
+    const tstText = 'Hello This is my Portfolio';
+
+    // React.useEffect( () => {
+    //     const pictures = fetchArtWork();
+    // }
+    // );
+
+    return (
+        <h1>
+            {tstText}
+        </h1>
+    );
+}
+
+export default Portfolio;
---
+++ src/pages/Egg.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = (props) => {
+    const tstText = 'Hello This is My About Page';
+
+    // React.useEffect( () => {
+    //     const pictures = fetchAboutData();
+    // }
+    // );
+
+    return (
+        <h1>
+            {tstText}
+        </h1>
+    );
+}
+
+export default About;
