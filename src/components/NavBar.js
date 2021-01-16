1e6e077e0ab8fe1272a4a836feb21cab71ccf5ab
Avi Agarwal <avi.agarwal117@gmail.com>
2021-01-16 02:41:21 -0800

    Adding Styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,21 @@
 import React from 'react'
-import '../index.css'
-import makeStyles from '@material-ui/core/styles/makeStyles';
 import { Navbar,Nav } from 'react-bootstrap'
-
-const useStyles = makeStyles({
-    navPadding: {paddingLeft: "40px", paddingTop: "15px"},
-    navText: {color: 'black', font: 'medium'}
-});
+import {titleStyle, navPadding, navText} from "../styles";
 
 const NavBar = () => {
-    const classes = useStyles();
     return (
         <div>
             <div className="row">
                 <div className="navStyle">
-                    <Navbar bg="light" variant="light" expand="lg" fixed="top" className={classes.navPadding}>
-                        <Navbar.Brand href="/">Samika</Navbar.Brand>
+                    <Navbar variant="light" expand="lg" fixed="top"  style={navPadding}>
+                        <Navbar.Brand href="/" style={titleStyle}>SAMIKA</Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="mr-auto">
-                                <Nav.Link href="/" className={classes.navText}>Portfolio</Nav.Link>
-                                <Nav.Link href="/blog">Blog</Nav.Link>
-                                <Nav.Link href="/contact">Contact</Nav.Link>
-                                <Nav.Link href="/about">About</Nav.Link>
+                                <Nav.Link style={navText} href="/">Portfolio</Nav.Link>
+                                <Nav.Link style={navText} href="/blog">Blog</Nav.Link>
+                                <Nav.Link style={navText} href="/contact">Contact</Nav.Link>
+                                <Nav.Link style={navText} href="/about">About</Nav.Link>
                             </Nav>
                         </Navbar.Collapse>
                     </Navbar>
---
+++ src/styles.js
@@ -0,0 +1,18 @@
+export const titleStyle = {
+    color: 'black',
+    fontFamily: 'Segoe UI',
+    fontWeight: 'bold',
+    fontSize: 30
+};
+
+export const navPadding = {
+    paddingLeft: "60px",
+    paddingTop: "15px",
+    backgroundColor: 'white'
+};
+
+export const navText = {
+    color: 'black',
+    fontFamily: 'Segoe UI',
+    fontSize: 20
+};
